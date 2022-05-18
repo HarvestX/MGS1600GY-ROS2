451e7f46d418e85dac92ea50c9c59607a5aea35f
m12watanabe1a <m12watanabe1a@gmail.com>
2022-05-19 00:32:36 +0900

    add launcher

---
+++ mgs1600gy_bringup/setup.py
@@ -0,0 +1,31 @@
+"""Setup."""
+
+from glob import glob
+
+from setuptools import setup
+
+package_name = 'mgs1600gy_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/{}/launch'.format(package_name),
+            glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='m12watanabe1a',
+    maintainer_email='m12watanabe1a@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
